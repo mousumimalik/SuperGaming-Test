07b91256cfeed357db30bc74c8ef87a7bc0c5069
Mousumi  Malik <62931934+mousumimalik@users.noreply.github.com>
2023-02-20 02:02:27 +0530

    change the client side base url

--- Constants.js
+++ Constants.js
@@ -5,7 +5,9 @@ const CONSTANTS = {
   LOGOUT: "/logouts",
   AUTH: "/auths",
   UPDATE_UNIT: "/units/",
-  CLIENT_BASE_URL: "http://127.0.0.1:5500",
+//   CLIENT_BASE_URL: "http://127.0.0.1:5500",
+  CLIENT_BASE_URL: "https://mousumimalik.github.io/SuperGaming-Test",
 };
 
 export default CONSTANTS;
+
